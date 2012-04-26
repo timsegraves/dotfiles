9e45439818d810385a030eaa9355f81b34fb1b73
timsegraves <timsegraves@gmail.com>
2012-04-26 15:44:58 -0600

    Adding aliases for tailing logs without asset pipeline crap

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,2 +1,4 @@
 alias reload!='. ~/.zshrc'
 alias v="/Applications/MacVim.app/Contents/MacOS/vim -g"
+alias taillog="tail -f log/development.log | grep -vE \"(^\s*$|asset)\""
+alias tailtestlog="tail -f log/test.log | grep -vE \"(^\s*$|asset)\""
