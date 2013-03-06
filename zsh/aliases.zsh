195d41e3ce80819a996368afbbe2ac3a2994a7b3
timsegraves <timsegraves@gmail.com>
2013-03-06 12:15:41 -0600

    Updates

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -2,3 +2,7 @@ alias reload!='. ~/.zshrc'
 alias v="/Applications/MacVim.app/Contents/MacOS/vim -g"
 alias taillog="tail -f log/development.log | grep -vE \"(^\s*$|asset)\""
 alias tailtestlog="tail -f log/test.log | grep -vE \"(^\s*$|asset)\""
+alias gem-unchanged="git update-index --assume-unchanged Gemfile && git update-index --assume-unchanged Gemfile.lock"
+alias gem-changed="git update-index --no-assume-unchanged Gemfile && git update-index --no-assume-unchanged Gemfile.lock"
+alias tunnel-ben-dev1.sendgrid.net="ssh -N -L7078:10.80.42.159:8177 root@ben-dev1.sendgrid.net"
+alias tunnel-suspicious-held="ssh -N -L7078:10.80.42.159:7077 root@ben-dev1.sendgrid.net"
