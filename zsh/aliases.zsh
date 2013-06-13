45cd838e1f4d675767ed865ad1e0dcbf6e73eaaa
timsegraves <timsegraves@gmail.com>
2013-06-13 15:57:24 -0500

    changes to dotfiles

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,8 +1,11 @@
 alias reload!='. ~/.zshrc'
 alias v="/Applications/MacVim.app/Contents/MacOS/vim -g"
+alias vi="/Applications/MacVim.app/Contents/MacOS/vim"
 alias taillog="tail -f log/development.log | grep -vE \"(^\s*$|asset)\""
 alias tailtestlog="tail -f log/test.log | grep -vE \"(^\s*$|asset)\""
 alias gem-unchanged="git update-index --assume-unchanged Gemfile && git update-index --assume-unchanged Gemfile.lock"
 alias gem-changed="git update-index --no-assume-unchanged Gemfile && git update-index --no-assume-unchanged Gemfile.lock"
 alias tunnel-ben-dev1.sendgrid.net="ssh -N -L7078:10.80.42.159:8177 root@ben-dev1.sendgrid.net"
 alias tunnel-suspicious-held="ssh -N -L7078:10.80.42.159:7077 root@ben-dev1.sendgrid.net"
+alias tunnel-irs="ssh root@dev.sendgrid.net -L 8111:10.9.180.104:8111 -N"
+alias tunnel-boss-staging="ssh segraves@stagejump.sjc1.sendgrid.net -L 8222:174.36.32.204:443 -N"
