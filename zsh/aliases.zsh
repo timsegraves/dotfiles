64c6925e7a5f526e98d15e149038cabc674d944e
Tim Segraves <tim.segraves@sendgrid.com>
2013-08-06 10:10:36 -0700

    Updates to teamocil and fixing weird rails issue

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -8,3 +8,4 @@ alias tunnel-ben-dev1.sendgrid.net="ssh -N -L7078:10.80.42.159:8177 root@ben-dev
 alias tunnel-suspicious-held="ssh -N -L7078:10.80.42.159:7077 root@ben-dev1.sendgrid.net"
 alias tunnel-irs="ssh root@dev.sendgrid.net -L 8111:10.9.180.104:8111 -N"
 alias tunnel-boss-staging="ssh segraves@stagejump.sjc1.sendgrid.net -L 8222:174.36.32.204:443 -N"
+alias rails="rails"
